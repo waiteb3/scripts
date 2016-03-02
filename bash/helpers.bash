0a6f76d43d4d271d666338f7dbdf1a8ea747bb79
bwaite-jaroop <bwaite@jaroop.com>
2016-03-02 16:08:04 -0500

    Add sbttz alias and fun with colors command

--- bash/helpers.bash
+++ bash/helpers.bash
@@ -6,3 +6,25 @@ ring_alarm() {
         sleep 1
     done
 }
+
+rainbow() {
+    echo -n "Can you taste it? can you feel it"
+    time=1
+    while [ $time -le 3 ]; do
+        echo -n ".."
+        sleep $time
+        : $((time++))
+    done
+    echo
+    print_color() {
+        color=$1
+        # echo -ne "${color}\t"
+        for mod in {1..9}; do
+            echo -en "\e[${color};${mod}mWARNING\e[0m "
+        done
+        echo
+    }
+    for color in {30..37}; do print_color $color; done
+    for color in {40..47}; do print_color $color; done
+}
+
