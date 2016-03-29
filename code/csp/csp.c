6503a8551a98e1438cedf05ab92d5ecdc5f215ff
bwaite-jaroop <bwaite@jaroop.com>
2016-03-29 09:26:03 -0400

    Small update to terrible csp idea

--- code/csp/csp.c
+++ code/csp/csp.c
@@ -3,19 +3,35 @@
 
 typedef struct {
     void (*fn)();
+    pthread_t self;
 } actor;
 
 
-void print() {
-    printf("test\n");
+void print(pthread_t pid) {
+    printf("From ID: %lu\n", pid);
+}
+
+void *wiring(void *argument) {
+    actor a;
+    a = *((actor *)argument);
+    a.self = pthread_self();
+    a.fn(a.self);
 }
 
 int main() {
-    actor a;
+    int NUM_THREADS = 5;
+    pthread_t threads[NUM_THREADS];
 
-    a.fn = print;
+    for ( int i = 0; i < NUM_THREADS; i++ ) {
+        actor a;
+        a.fn = print;
 
-    a.fn();
+        int s = pthread_create(&threads[i], NULL, wiring, &a);
+    }
+
+    for ( int i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
 
     return 0;
 }
