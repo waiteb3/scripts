a4aad2e1ad4ced78dafe479938861479bea000d1
bwaite-jaroop <bwaite@jaroop.com>
2016-04-15 13:28:45 -0400

    Fix simple http server to print the right URL and parse flags correctly

--- code/shttpd/shttpd.go
+++ code/shttpd/shttpd.go
@@ -4,18 +4,27 @@ import (
     "flag"
     "log"
     "net/http"
+    "strconv"
 )
 
 
 func main() {
-    var ADDR = flag.String("-addr", ":3000", "address")
-    var DIR = flag.String("-dir", ".", "directory")
+    var ADDR = flag.String("addr", "3000", "address (")
+    var DIR = flag.String("dir", ".", "directory")
     flag.Parse()
 
+    var message = `Serving "` + *DIR + `" on http://`
+
     if (*ADDR)[0] == ':' {
-        log.Println("listening at http://localhost:" + *ADDR)
+        log.Println("listening at http://localhost" + *ADDR)
+        message += "localhost" + *ADDR
+    } else if _, err := strconv.Atoi(*ADDR); err == nil {
+        *ADDR = ":" + *ADDR
+        message += "localhost" + *ADDR
     } else {
-        log.Println("listening at http://" + *ADDR)
+        message += *ADDR
     }
+
+    log.Println(message)
     log.Fatalln(http.ListenAndServe(*ADDR, http.FileServer(http.Dir(*DIR))))
 }
